a0ad87eed210ebcf17935b531406efdd839e48e4
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2019-11-01 06:54:18 +0530

    initial commit

---
+++ src/main/java/com/knowledge/share/quiz/knowledgesharequizbot/KnowledgeShareQuizBotApplication.java
@@ -0,0 +1,13 @@
+package com.knowledge.share.quiz.knowledgesharequizbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeShareQuizBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeShareQuizBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowledge/share/quiz/knowledgesharequizbot/KnowledgeShareQuizBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledge.share.quiz.knowledgesharequizbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeShareQuizBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

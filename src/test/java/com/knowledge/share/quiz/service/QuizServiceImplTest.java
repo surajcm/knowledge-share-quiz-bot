ec8a531db38a041c97e5b8803a4866e11b1ec4a4
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2020-08-14 22:52:53 +0530

    first test case

---
+++ src/test/java/com/knowledge/share/quiz/service/QuizServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.knowledge.share.quiz.service;
+
+import com.knowledge.share.quiz.dao.QuizDAO;
+import com.knowledge.share.quiz.dao.QuizDAOImpl;
+import com.knowledge.share.quiz.dao.entity.Quiz;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.powermock.reflect.Whitebox;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {QuizDAO.class})
+class QuizServiceImplTest {
+    private final QuizService quizService = new QuizServiceImpl();
+    private final QuizDAO quizDAO = Mockito.mock(QuizDAOImpl.class);
+
+    @Test
+    void verifyGenerateRandomQuiz() {
+        Whitebox.setInternalState(quizService, "quizDAO", quizDAO);
+        when(quizDAO.countOfQuizzes()).thenReturn(2L);
+        when(quizDAO.fetchQuizFromId(anyLong()))
+                .thenReturn(Optional.of(Mockito.mock(Quiz.class)));
+        Quiz quiz = quizService.generateRandomQuiz();
+        Assertions.assertNotNull(quiz);
+    }
+}

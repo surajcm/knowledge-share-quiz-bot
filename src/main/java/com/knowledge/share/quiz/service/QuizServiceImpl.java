bdf5a488f1d20fa275ad2c52dd05889c6a233494
Suraj Muraleedharan <surajcm@gmail.com>
2024-03-16 09:40:55 +0530

    Fixing the build failure

--- src/main/java/com/knowledge/share/quiz/service/QuizServiceImpl.java
+++ src/main/java/com/knowledge/share/quiz/service/QuizServiceImpl.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -19,28 +18,20 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public Quiz generateRandomQuiz() {
-        Quiz quiz = null;
         logger.info("Inside generateRandomQuizNewImplementation quiz !!!");
         long count = quizDAO.countOfQuizzes();
         int random = new Random().nextInt((int) count - 1);
         random++;
         logger.info("Random number is :{}", random);
-        Optional<Quiz> quizOptional = quizDAO.fetchQuizFromId((long) random);
-        if (quizOptional.isPresent()) {
-            quiz = quizOptional.get();
-        }
-        return quiz;
+        var quizOptional = quizDAO.fetchQuizFromId((long) random);
+        return quizOptional.orElse(null);
     }
 
     @Override
     public Quiz getQuizFromId(final String id) {
-        Quiz quiz = null;
         logger.info("Inside getQuizFromId  !!!");
         Long idValue = Long.valueOf(id);
-        Optional<Quiz> quizOptional = quizDAO.fetchQuizFromId(idValue);
-        if (quizOptional.isPresent()) {
-            quiz = quizOptional.get();
-        }
-        return quiz;
+        var quizOptional = quizDAO.fetchQuizFromId(idValue);
+        return quizOptional.orElse(null);
     }
 }

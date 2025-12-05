2110437da039a8cb7c389344d1eb115d360c830f
Suraj Muraleedharan <surajcm@gmail.com>
2025-12-05 07:02:26 +0530

    Upgrading to spring boot 4

--- src/main/java/com/knowledge/share/KnowledgeShareQuizBotApplication.java
+++ src/main/java/com/knowledge/share/KnowledgeShareQuizBotApplication.java
@@ -2,7 +2,7 @@ package com.knowledge.share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
